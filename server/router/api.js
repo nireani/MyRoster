607aef89015ac5bf815a8725301e6ff86006f505
Nir <nireani@gmail.com>
2021-04-13 12:16:21 +0300

    fixedGetBug

--- server/router/api.js
+++ server/router/api.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const urllib = require('urllib')
+const request = require('request')
 
 const router = express()
 
@@ -11,49 +11,50 @@ const teamToIDs = {
     "suns": "1610612756"
 }
 let players = []
- router.get('/teams/:teamName', function(req,res){
-     
-  urllib.request('http://data.nba.net/10s/prod/v1/2018/players.json').then( (result)=>{
-  const teamNum = teamToIDs[req.params.teamName.toLocaleLowerCase()]
-  if(teamNum){
-    const p =JSON.parse(result.data.toString())
-    const pArr = p.league.standard
-const newArr = pArr.map(pl=> {return {pos: pl.pos , jersey:pl.jersey,active :pl.isActive, fname:pl.firstName, lname:pl.lastName, teamId:pl.teamId}})
-const teamArr = newArr.filter(tp=> tp.active&&tp.teamId==teamNum)
-    players = teamArr
+router.get('/teams/:teamName', function (req, resg) {
+    const teamNum = teamToIDs[req.params.teamName.toLocaleLowerCase()]
 
-  }else{
-players = "1"
-}
-      })
-
-   res.send(players)
+    if (teamNum) {
+        request('http://data.nba.net/10s/prod/v1/2018/players.json',function(req,res){
+            const parsedTeam = JSON.parse(res.body).league.standard
+            const mappedTeamArr = parsedTeam.map(pl => { return { pos: pl.pos, jersey: pl.jersey, active: pl.isActive, fname: pl.firstName, lname: pl.lastName, teamId: pl.teamId } })
+            const filteredTeamArr = mappedTeamArr.filter(tp => tp.active && tp.teamId == teamNum)
+            players = filteredTeamArr
+            resg.send(players)
+        })
+    } else {
+        players = "1"
+        resg.send(players)
+    }
 })
 
+
+
+
 router.put('/team', function (req, res) {
-    const obj= req.query
-    teamToIDs[obj.teamName] = obj.teamId    
-   
-     
-     res.send(teamToIDs)
- })
+    const obj = req.query
+    teamToIDs[obj.teamName] = obj.teamId
 
- let dreamTeam=[]
-router.get('/dreamTeam', function (req, res) { 
-     
-     res.send(dreamTeam)
- })
-router.post('/roster', function (req, res) { 
-     let fname = req.query.fname
-let lname = req.query.lname
-let jersey = req.query.jersey
-let pos = req.query.pos
-let obj = {fname:fname,lname:lname,pos:pos,jersey:jersey}
-if(dreamTeam.length<5){
-    dreamTeam.push(obj)
-}
 
-     res.send(dreamTeam)
- })
+    res.send(teamToIDs)
+})
+
+let dreamTeam = []
+router.get('/dreamTeam', function (req, res) {
+
+    res.send(dreamTeam)
+})
+router.post('/roster', function (req, res) {
+    let fname = req.query.fname
+    let lname = req.query.lname
+    let jersey = req.query.jersey
+    let pos = req.query.pos
+    let obj = { fname: fname, lname: lname, pos: pos, jersey: jersey }
+    if (dreamTeam.length < 5) {
+        dreamTeam.push(obj)
+    }
+
+    res.send(dreamTeam)
+})
 
 module.exports = router
